a6644108899f08eb34cd60c24cac501ae68f1f96
For3v3rInFriendZone <strile93@gmail.com>
2016-05-17 09:56:45 +0200

    Pregazena funkcionalnost za dijagram, pa sam je vratio.

--- Kod/client/primer02.js
+++ Kod/client/primer02.js
@@ -80,6 +80,10 @@
       		$location.path('/projekat/'+ projEntry._id + '/zadaci');
       	}
 
+      	$scope.izvestaji = function (projEntry) {
+      		$location.path('/projekat/'+ projEntry._id + '/izvestaji');
+      	}
+
 
     	//za korisnike
     	var KorEntry = $resource('/api/korisnik');
@@ -379,82 +383,77 @@
 
 	var ZadaciKorisnicimaCtrl = function ($scope, $http, $resource, $stateParams, $location) 
 	{
-		$scope.podaci = [];
-		
-		$http.get('/api/korisnik/')
-		.success(function(data, status, headers)
+
+		$scope.izvestaj1 = function () 
 		{
-			
-			console.log(data);
-			for (var i = 0; i < data.length; i++) 
-			{
-				$scope.podaci.push({
-						   key:data[i].ime + ' ' + data[i].prezime, 
-						   y: data[i].zadatak.length
-				});
-				
-			}
-
-
-			//sam kod za pravljenje dijagrama
-
-			var height = 350;
-		    var width = 350;
-
-		    nv.addGraph(function() {
-		        var chart = nv.models.pieChart()
-		            .x(function(d) { return d.key })
-		            .y(function(d) { return d.y })
-		            .width(width)
-		            .height(height)
-		            .showTooltipPercent(true);
-
-		        d3.select("#test1")
-		            .datum($scope.podaci)
-		            .transition().duration(1200)
-		            .attr('width', width)
-		            .attr('height', height)
-		            .call(chart);
-
-		        return chart;
-		    });
-
-		    nv.addGraph(function() {
-		        var chart = nv.models.pieChart()
-		            .x(function(d) { return d.key })
-		            .y(function(d) { return d.y })
-		            //.labelThreshold(.08)
-		            //.showLabels(false)
-		            .color(d3.scale.category20().range().slice(8))
-		            .growOnHover(false)
-		            .labelType('value')
-		            .width(width)
-		            .height(height);
-
-		        // disable and enable some of the sections
-		        var is_disabled = false;
-		        setInterval(function() {
-		            chart.dispatch.changeState({disabled: {2: !is_disabled, 4: !is_disabled}});
-		            is_disabled = !is_disabled;
-		        }, 3000);
-
-		        return chart;
-		    });
-		});
-
+			$scope.podaci = [];
 		
+			if(!angular.equals({}, $stateParams))
+			{
 
-		var testdata2 = [
-	        {key: "One", y: 5},
-	        {key: "Two", y: 2},
-	        {key: "Three", y: 9},
-	        {key: "Four", y: 7},
-	        {key: "Five", y: 4},
-	        {key: "Six", y: 3},
-	        {key: "Seven", y: 0.5}
-	    ];
+				$http.get('/api/projekat/' + $stateParams.id)
+				.success(function(data, status, headers)
+				{
+					
+					for (var i = 0; i < data.korisnici.length; i++) 
+					{
+						$scope.podaci.push({
+								   key:data.korisnici[i].ime + ' ' + data.korisnici[i].prezime, 
+								   y: data.korisnici[i].zadatak.length
+						});
+						
+					}
 
-	    
+
+					//sam kod za pravljenje dijagrama
+
+					var height = 350;
+				    var width = 350;
+
+				    nv.addGraph(function() {
+				        var chart = nv.models.pieChart()
+				            .x(function(d) { return d.key })
+				            .y(function(d) { return d.y })
+				            .width(width)
+				            .height(height)
+				            .showTooltipPercent(true);
+
+				        d3.select("#test1")
+				            .datum($scope.podaci)
+				            .transition().duration(1200)
+				            .attr('width', width)
+				            .attr('height', height)
+				            .call(chart);
+
+				        return chart;
+				    });
+
+				    nv.addGraph(function() {
+				        var chart = nv.models.pieChart()
+				            .x(function(d) { return d.key })
+				            .y(function(d) { return d.y })
+				            //.labelThreshold(.08)
+				            //.showLabels(false)
+				            .color(d3.scale.category20().range().slice(8))
+				            .growOnHover(false)
+				            .labelType('value')
+				            .width(width)
+				            .height(height);
+
+				        // disable and enable some of the sections
+				        var is_disabled = false;
+				        setInterval(function() {
+				            chart.dispatch.changeState({disabled: {2: !is_disabled, 4: !is_disabled}});
+				            is_disabled = !is_disabled;
+				        }, 3000);
+
+				        return chart;
+				    });
+
+				    $location.path('/projekat/' + data._id + '/izvestaji/izvestaj1');
+				});
+			}
+		}		
 	}
 
 
@@ -529,10 +528,14 @@
 	      templateUrl: 'komentari-unos.html',
 	      controller: 'komentariCtrl'
 	    })
-	    .state('izvestajZadaci1', {
-	      url: '/izvestaj1', 
+	    .state('izvestaji', {
+	      url: '/projekat/:id/izvestaji', 
 	      templateUrl: 'izvestaj-zadaci-dodeljeni-projekat.html'
 	    //  controller: 'komentariCtrl'
+	    })
+	    .state('izvestaji.izvestaj1', {
+	      url: '/izvestaj1', 
+	      templateUrl: 'izvestaj-izvestaj1.html'
 	    })   
 	    .state('korDash', {
 	      url: '/korisnik/:id', 
