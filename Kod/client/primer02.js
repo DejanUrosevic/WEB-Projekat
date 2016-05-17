c06be87e9110354f44b7b23f2d3eb212df713c09
aleksandarmanasijevic <manasbre@gmail.com>
2016-05-17 03:38:50 +0200

    Uradjen pregled projekata na kojima je korisnik angazovan.

--- Kod/client/primer02.js
+++ Kod/client/primer02.js
@@ -20,12 +20,33 @@
 			user.$delete(loadEntries);
 		}
 
+		var KorisnikSesija;
+		
 		if(!angular.equals({}, $stateParams))
 		{
 			var KorEntry2 = $resource('/api/korisnik/:_id');
 			var korEntryId = $stateParams.id;
-    		$scope.User = KorEntry2.get({_id:korEntryId});
+    		KorisnikSesija = KorEntry2.get({_id:korEntryId});
 		}
+
+		var ProjEntry = $resource('/api/projekat/:id',{id: '@_id'});
+		
+		$scope.UserProjEntries = [];
+
+		var ProjEntries = ProjEntry.query(function(){
+			for (var i = ProjEntries.length - 1; i >= 0; i--) {
+			var ProjKorisnici = ProjEntries[i].korisnici;
+			// $scope.UserProjEntries.push(ProjEntries[i]);
+			for (var j = ProjKorisnici.length - 1; j >= 0; j--) {
+				if(KorisnikSesija.email == ProjKorisnici[j].email){
+					$scope.UserProjEntries.push(ProjEntries[i]);
+				}
+			}
+		}
+
+
+		});	
+		
 	};
 
 	var projekatCtrl = function ($scope, $http, $resource, $location, $stateParams) 
@@ -57,9 +78,6 @@
 
     	$scope.pregledZadataka = function (projEntry) {
       		$location.path('/projekat/'+ projEntry._id + '/zadaci');
-      	}
-      	$scope.izvestaji = function (projEntry) {
-      		$location.path('/projekat/'+ projEntry._id + '/izvestaji');
       	}
 
 
@@ -361,79 +379,81 @@
 
 	var ZadaciKorisnicimaCtrl = function ($scope, $http, $resource, $stateParams, $location) 
 	{
-
-		$scope.izvestaj1 = function () 
+		$scope.podaci = [];
+		
+		$http.get('/api/korisnik/')
+		.success(function(data, status, headers)
 		{
-			$scope.podaci = [];
-		
-			if(!angular.equals({}, $stateParams))
+			
+			console.log(data);
+			for (var i = 0; i < data.length; i++) 
 			{
-
-				$http.get('/api/projekat/' + $stateParams.id)
-				.success(function(data, status, headers)
-				{
-					
-					for (var i = 0; i < data.korisnici.length; i++) 
-					{
-						$scope.podaci.push({
-								   key:data.korisnici[i].ime + ' ' + data.korisnici[i].prezime, 
-								   y: data.korisnici[i].zadatak.length
-						});
-						
-					}
-
-
-					//sam kod za pravljenje dijagrama
-
-					var height = 350;
-				    var width = 350;
-
-				    nv.addGraph(function() {
-				        var chart = nv.models.pieChart()
-				            .x(function(d) { return d.key })
-				            .y(function(d) { return d.y })
-				            .width(width)
-				            .height(height)
-				            .showTooltipPercent(true);
-
-				        d3.select("#test1")
-				            .datum($scope.podaci)
-				            .transition().duration(1200)
-				            .attr('width', width)
-				            .attr('height', height)
-				            .call(chart);
-
-				        return chart;
-				    });
-
-				    nv.addGraph(function() {
-				        var chart = nv.models.pieChart()
-				            .x(function(d) { return d.key })
-				            .y(function(d) { return d.y })
-				            //.labelThreshold(.08)
-				            //.showLabels(false)
-				            .color(d3.scale.category20().range().slice(8))
-				            .growOnHover(false)
-				            .labelType('value')
-				            .width(width)
-				            .height(height);
-
-				        // disable and enable some of the sections
-				        var is_disabled = false;
-				        setInterval(function() {
-				            chart.dispatch.changeState({disabled: {2: !is_disabled, 4: !is_disabled}});
-				            is_disabled = !is_disabled;
-				        }, 3000);
-
-				        return chart;
-				    });
-
-				    $location.path('/projekat/' + data._id + '/izvestaji/izvestaj1');
+				$scope.podaci.push({
+						   key:data[i].ime + ' ' + data[i].prezime, 
+						   y: data[i].zadatak.length
 				});
+				
 			}
-		}
+
+
+			//sam kod za pravljenje dijagrama
+
+			var height = 350;
+		    var width = 350;
+
+		    nv.addGraph(function() {
+		        var chart = nv.models.pieChart()
+		            .x(function(d) { return d.key })
+		            .y(function(d) { return d.y })
+		            .width(width)
+		            .height(height)
+		            .showTooltipPercent(true);
+
+		        d3.select("#test1")
+		            .datum($scope.podaci)
+		            .transition().duration(1200)
+		            .attr('width', width)
+		            .attr('height', height)
+		            .call(chart);
+
+		        return chart;
+		    });
+
+		    nv.addGraph(function() {
+		        var chart = nv.models.pieChart()
+		            .x(function(d) { return d.key })
+		            .y(function(d) { return d.y })
+		            //.labelThreshold(.08)
+		            //.showLabels(false)
+		            .color(d3.scale.category20().range().slice(8))
+		            .growOnHover(false)
+		            .labelType('value')
+		            .width(width)
+		            .height(height);
+
+		        // disable and enable some of the sections
+		        var is_disabled = false;
+		        setInterval(function() {
+		            chart.dispatch.changeState({disabled: {2: !is_disabled, 4: !is_disabled}});
+		            is_disabled = !is_disabled;
+		        }, 3000);
+
+		        return chart;
+		    });
+		});
+
 		
-		
+
+		var testdata2 = [
+	        {key: "One", y: 5},
+	        {key: "Two", y: 2},
+	        {key: "Three", y: 9},
+	        {key: "Four", y: 7},
+	        {key: "Five", y: 4},
+	        {key: "Six", y: 3},
+	        {key: "Seven", y: 0.5}
+	    ];
+
 	    
 	}
 
@@ -474,14 +494,6 @@
 	      templateUrl: 'zad-unos.html'
 	    //  controller: 'projekatCtrl'
 	    })
-	    .state('izvestaj', {
-	      url: '/projekat/:id/izvestaji', 
-	      templateUrl: 'izvestaj-zadaci-dodeljeni-projekat.html'
-	    })
-	    .state('izvestaj.izvestaj1', {
-	      url: '/izvestaj1', 
-	      templateUrl: 'izvestaj-izvestaj1.html'
-	    })
 	    .state('spisakUserProjekat', {
 	      url: '/projekat/:id/korisnik', 
 	      templateUrl: 'spisak-korisnika-projekat.html'
@@ -516,7 +528,12 @@
 	      url: '/projekat/:id/zadatak/:id2/noviKomentar', 
 	      templateUrl: 'komentari-unos.html',
 	      controller: 'komentariCtrl'
-	    }) 
+	    })
+	    .state('izvestajZadaci1', {
+	      url: '/izvestaj1', 
+	      templateUrl: 'izvestaj-zadaci-dodeljeni-projekat.html'
+	    //  controller: 'komentariCtrl'
+	    })   
 	    .state('korDash', {
 	      url: '/korisnik/:id', 
 	      templateUrl: 'korisnik-dashboard.html',
